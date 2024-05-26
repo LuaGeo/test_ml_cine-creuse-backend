4a36d513d2b5ae027cc871ce97be52bd3322b4f1
luageo <ldeoliveiratech@gmail.com>
2024-05-26 15:57:11 +0200

    popular_movies.py deleted

--- app.py
+++ app.py
@@ -20,12 +20,10 @@ get_db(app)
 # Import routes
 from routes.user import setup_user_routes, setup_favorite_movies_routes
 from routes.recommendations import setup_recommendations_routes
-from routes.popular_movies import setup_popular_movies_routes
 
 setup_user_routes(app)
 setup_favorite_movies_routes(app)
 setup_recommendations_routes(app)
-setup_popular_movies_routes(app)
 
 if __name__ == "__main__":
     app.run(debug=False)
