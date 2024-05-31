01c4813b256232143f6a82cf44bd4cd165d46546
luageo <ldeoliveiratech@gmail.com>
2024-05-31 10:36:31 +0200

    .parquet link fixed

--- models/recommendation_model.py
+++ models/recommendation_model.py
@@ -4,11 +4,11 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 import numpy as np
 
 
-# link = "/Users/lua/wild/project2/data_cleaned_ml_with_original_columns2.csv"
-# df = pd.read_csv(link)
-
-link = "/Users/lua/wild/project2/test_ml_cine-creuse-backend/data/data_cleaned_ml_with_original_columns.parquet"
+link = "https://raw.githubusercontent.com/LuaGeo/test_ml_cine-creuse-backend/main/data/data_cleaned_ml_with_original_columns.parquet"
 df = pd.read_parquet(link)
+
+# link = "/Users/lua/wild/project2/test_ml_cine-creuse-backend/data/data_cleaned_ml_with_original_columns.parquet"
+# df = pd.read_parquet(link)
 
 df = df.dropna()
 
